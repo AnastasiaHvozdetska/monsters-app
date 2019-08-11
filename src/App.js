21fca158dad70ae28a9ed2d808e1a6ef314d7eca
Anastasia <anastasia9297@gmail.com>
2019-08-11 23:31:13 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App () {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+class App extends Component {
+  constructor() {
+    super();
 
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein'
+        },
+        {
+          name: 'Dracula'
+        },
+        {
+          name: 'Zombie'
+        }
+      ]
+    };
+  };
+
+  handleChangeText = () => {
+
+  }
+
+  render () {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map((monster, index) => <h1 key={index}>{monster.name}</h1>)
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
